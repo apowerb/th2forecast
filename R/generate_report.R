8f3ac32882fdcbd355ed328237a6adee32f71edd
Fair Analytics <doudou_ulb@hotmail.com>
2024-07-04 11:36:38 +0200

    relative path report
    relative path solution for the report

--- R/generate_report.R
+++ R/generate_report.R
@@ -1,6 +1,9 @@
 #' @export
 generate_report_forecast <- function(dataset_input, kpi, models, column_kpi){
-  rmd_report_file <- "./inst/test_models.Rmd"
+
+  path_rmarkdown <- system.file("rmarkdown", package = "th2forecast")
+
+  rmd_report_file <-  paste0(path_rmarkdown, "/models_stability_test.Rmd")
   report_params <- list(data = dataset_input, kpi = kpi, models = models, column_kpi = column_kpi)
 
   rmarkdown::render(input = rmd_report_file, params = report_params, output_dir = "./reports" )
