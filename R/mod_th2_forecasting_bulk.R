80ffe4abccaf7da0fe39704e5fe48768666978e5
Farid Azouaou <farid.azouaou@thaink2.com>
2024-11-05 16:23:13 +0100

    bugfix , lines instead of dots

--- R/mod_th2_forecasting_bulk.R
+++ R/mod_th2_forecasting_bulk.R
@@ -108,6 +108,7 @@ forecast_train_mod_server2 <- function(id, div_width = "col-xs-6 col-sm-12 col-m
       data <- th2ml::load_ml_models_for_api_s3(target_model_name = input$selected_bi_data, s3_bucket = s3_bucket, s3_prefix = s3_prefix)%>%
         janitor::clean_names()
       data_is_loaded(input$selected_bi_data)
+      data2 <<- data
       return(data)
     })
     output$selected_bi_data <- renderUI({
@@ -122,7 +123,9 @@ forecast_train_mod_server2 <- function(id, div_width = "col-xs-6 col-sm-12 col-m
     data_diag <- reactive({
       req(tisefka())
       req(input$load_ml_data)
-      SaldaeDataExplorer::data_diagnosis_f(tisefka())
+      dt_diag <- SaldaeDataExplorer::data_diagnosis_f(tisefka())
+      dt_diag2 <<- dt_diag
+      return(dt_diag)
     })
 
     categoricals_unique_values <- reactive({
