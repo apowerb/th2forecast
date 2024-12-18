c9563292ef9a909258382e6b432b0936ab5390d2
Farid Azouaou <farid.azouaou@thaink2.com>
2024-12-18 08:10:07 +0100

    forecasting as a service v1 add body inputs

--- R/th2forecast_api_helpers.R
+++ R/th2forecast_api_helpers.R
@@ -1,15 +1,12 @@
 #' th2forecast_forecast_api
 #' @export
-th2forecast_forecast_api <- function(input_data,group_target = NULL, fcast_horizon = 30, target_var,date_var ){
+th2forecast_forecast_api <- function(input_data,group_target = NULL,base_url ,fcast_horizon = 30, target_var,date_var , models_list = NULL){
 
   token <- jose::jwt_claim(user= "test@thaink2.com", scope= "readonly")
   #
   encrypted_token <- th2marketplace::th2_api_token_generator(jwt_token = token, target_action= "encrypt")
   #
   # decrypted_token <- th2_api_token_generator(jwt_token = encrypted_token, target_action= "decrypt")
-
-  base_url <- "http://127.0.0.1:3838/"
-  base_url <- "https://apis-dev.thaink2.fr/"
   end_point <- "thaink2/forecasting"
 
   req_body <- list(
@@ -18,7 +15,7 @@ th2forecast_forecast_api <- function(input_data,group_target = NULL, fcast_horiz
     "group_target"= group_target,
     "target_var" = target_var,
     "date_var" = date_var,
-    "fcast_model" = "xgboost"
+    "models_list" = models_list
   )
 
   req_url <- glue::glue("{base_url}{end_point}")
@@ -28,17 +25,22 @@ th2forecast_forecast_api <- function(input_data,group_target = NULL, fcast_horiz
     httr2::req_method("POST")
 
   forecast_resp <- forecast_req%>%
-    httr2::req_perform(verbosity = 3)
+    httr2::req_perform(verbosity = 0)
 
   forecast_result <- forecast_resp%>%
     httr2::resp_body_json()
   return(forecast_result)
 }
-#
+
+
+# base_url <- "http://127.0.0.1:3838/"
+# base_url <- "https://apis-dev.thaink2.fr/"
 # fcast_res <- th2forecast_forecast_api(
 #   input_data = main_dataset2,
+#   base_url = base_url,
 #   fcast_horizon = 30,
 #   target_var = "y",
 #   date_var = "ds",
-#   group_target = "unique_id"
+#   group_target = "unique_id",
+#   models_list = c("arima")
 # )
